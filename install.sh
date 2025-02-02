33c3054e322460a97a679fbe272ae519818d07f8
ozzy8160 <RYANPCTECH81@GMAIL.COM>
2025-02-02 15:37:29 -0500

    Update install.sh
    rewrite of install script to symlink everthing to correct location and make a backup of nixos config

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-ln -s ~/.dotfiles/* ~/.config
+ln -s ~/.dotfiles/hypr ~/.config
+ln -s ~/.dotfiles/waybar ~/.config
+ln -s ~/.dotfiles/starship/startship.toml ~/.config
+ln -s ~/.dotfiles/fastfetch ~/.config
+ln -s ~/.dotfiles/nvim ~/.config
+
+sudo mv /etc/nixos/* ~/.dotfiles/bak
+ln -s ~/.dotfiles/flakes/* /etc/nixos/
+
+ln -s ~/.dotfiles/bashrc/.bashrc ~
