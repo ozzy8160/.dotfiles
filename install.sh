df4ad42beae5d4c261b2c3886e9bb53e56f82634
ozzy8160 <RYANPCTECH81@GMAIL.COM>
2025-02-10 21:15:10 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+cd ~/.config
+
+unlink hypr
+unlink waybar
+unlink fastfetch
+unlink nvim
+unlink starship.toml
+unlink kitty
+unlink flakes
+unlink .bashrc
+
 ln -s ~/.dotfiles/hypr ~/.config
 ln -s ~/.dotfiles/waybar ~/.config
 ln -s ~/.dotfiles/starship/startship.toml ~/.config
